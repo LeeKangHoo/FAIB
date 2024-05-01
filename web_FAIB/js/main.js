15309ebd015de7f0cb7f1483f9b09dfb58658139
kkanghoo <114421949+LeeKangHoo@users.noreply.github.com>
2024-05-01 20:41:38 +0900

    ud

--- web_FAIB/js/main.js
+++ web_FAIB/js/main.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const fileupload_router = require('./fileupload');
-const analyse_router = require('./analyse');
+//const analyse_router = require('./analyse');
 //const login_router = require('./login');
 //const profile_router = require('./profile');
 const app = express();
 
 
 app.use('/upload',fileupload_router);
-app.use('/analyse',analyse_router);
+//app.use('/analyse',analyse_router);
 
 app.listen(19132, () =>{
   console.log('@@@@@@@@  FAIB  @@@@@@@@');
