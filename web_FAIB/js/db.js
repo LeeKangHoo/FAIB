a6c802f794b45bd5cc2944b2aeb70041bb23ab25
LeeKangHoo <protection0828@gmail.com>
2024-06-13 01:15:32 +0900

    update

---
+++ web_FAIB/js/db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+const dbInfo = mysql.createConnection({
+    host:'localhost',
+    user:'kkanghoo',
+    password:'kkanghoo!@#0828',
+    port:3333,
+    database:'client'
+});
+//id : lkh, pw : test
+
+module.exports = {
+    init: function () {
+        return mysql.createConnection(dbInfo);
+    },
+    connect: function(conn) {
+        conn.connect(function(err) {
+            if(err) console.error('mysql 연결 에러 : ' + err);
+            else console.log('mysql 연결 성공');
+        });
+    }
+};
--- web_FAIB/js/server.js
+++ web_FAIB/js/server.js
@@ -3,22 +3,28 @@ const fileupload_router = require('./fileupload');
 const analyse_router = require('./analyse');
 const filedownload_router = require('./filedownload');
 const login_router = require('./login');
-//const profile_router = require('./profile');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'kanghoo!secret', // 적절한 비밀 키로 변경하세요.
+    resave: false,
+    saveUninitialized: true
+}));
 
-app.use('/upload',fileupload_router);
-app.use('/analyse',analyse_router);
-app.use('/download',filedownload_router);
+app.use('/upload', fileupload_router);
+app.use('/analyse', analyse_router);
+app.use('/download', filedownload_router);
 app.use('/login', login_router);
 
-
-app.listen(19132, () =>{
-  console.log('@@@@@@@@  FAIB  @@@@@@@@');
-  console.log('server vulnurability analys project');
-  console.log('LeeKangHoo(Nine)');
-  console.log('Server port 19132');
-  console.log('@@@@@@@@@@@@@@@@@@@@@@@@');
+app.listen(19132, () => {
+    console.log('@@@@@@@@  FAIB  @@@@@@@@');
+    console.log('server vulnerability analysis project');
+    console.log('LeeKangHoo(Nine)');
+    console.log('Server port 19132');
+    console.log('@@@@@@@@@@@@@@@@@@@@@@@@');
 });
-
