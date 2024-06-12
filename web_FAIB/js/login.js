22287bc9f3119638829371d7bc70033281b2710a
LeeKangHoo <protection0828@gmail.com>
2024-06-13 01:25:15 +0900

    update

--- web_FAIB/js/login.js
+++ web_FAIB/js/login.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const db = require('./db');
 
-router.get('/',(req,res) => {
-    //const filepath = path.join(__dirname + "/../public/analyse.html");
-    //res.sendFile(filepath);
+// 로그인 페이지를 렌더링하는 라우터
+router.get('/', (req, res) => {
     const filepath = path.join(__dirname, '../public/login.html');
     res.sendFile(filepath);
-    console.log("test");
+    console.log("로그인 페이지 로드");
+});
+
+// 로그인 요청을 처리하는 라우터
+router.post('/', (req, res) => {
+    const { username, password } = req.body;
+    const conn = db.init();
+
+    conn.query('SELECT * FROM users WHERE username = ?', [username], (err, results) => {
+        if (err) {
+            console.error('MySQL 에러: ', err);
+            res.status(500).send('서버 에러');
+        } else if (results.length === 0) {
+            res.status(401).send('유효하지 않은 사용자 이름');
+        } else {
+            const user = results[0];
+            if (password === user.password) {
+                req.session.user = user;
+                res.send('로그인 성공');
+            } else {
+                res.status(401).send('유효하지 않은 비밀번호');
+            }
+        }
+    });
 });
 
 module.exports = router;
